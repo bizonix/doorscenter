6539f6227028f379036955f56aedac0da9cb6c71
alex borisov <alex@altstone.com>
2011-03-29 22:57:48 +0400

    развитие и документация

--- doorsadmin/management/commands/cron.py
+++ doorsadmin/management/commands/cron.py
@@ -8,15 +8,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dd = datetime.date.today()
         dt = datetime.datetime.now()
-        '''Генерируем дорвеи'''
-        for p in DoorwaySchedule.objects.filter(active=True).all(): 
-            if (p.dateStart <= dd) and ((p.dateEnd==None) or (p.dateEnd >= dd)):
-                p.GenerateDoorways()
         '''Сниппеты'''
         for p in SnippetsSet.objects.filter(active=True).all():
             if (p.dateLastParsed==None) or (p.dateLastParsed + datetime.timedelta(0, p.interval*60*60, 0) < dt):
                 p.stateManaged = 'new'
                 p.save()
+        '''Генерируем дорвеи'''
+        for p in DoorwaySchedule.objects.filter(active=True).all(): 
+            if (p.dateStart <= dd) and ((p.dateEnd==None) or (p.dateEnd >= dd)):
+                p.GenerateDoorways()
         '''Проверяем активность агентов'''
         for p in Agent.objects.filter(active=True).all():
             if (p.dateLastPing!=None) and (p.dateLastPing + datetime.timedelta(0, p.interval*60*60, 0) < dt):
