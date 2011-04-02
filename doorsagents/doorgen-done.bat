44d1972b75e2b549fa899e06fa9ab626622b6f57
alex borisov <alex@altstone.com>
2011-04-02 19:18:32 +0400

    развитие

--- doorsagents/doorgen-done.bat
+++ doorsagents/doorgen-done.bat
@@ -1,2 +1,2 @@
 @c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\doorgen.py done >> C:\Work\doorscenter\doorsagents\doorgen.log
-@c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\doorgen.py cron >> C:\Work\doorscenter\doorsagents\doorgen.log
+C:\Work\doorscenter\doorsagents\doorgen-cron.bat
