92afd196154da4a7419e94adfa176ed9a54ef838
alex borisov <alex@altstone.com>
2011-04-25 10:27:26 +0400

    безбанный парсинг гугла

--- doorsagents/3rdparty/parser/parse.bat
+++ doorsagents/3rdparty/parser/parse.bat
@@ -1,4 +1,4 @@
 set path=%path%;C:\Work\snippets\parser
-C:\Work\snippets\parser\lib\php -c C:\Work\snippets\parser\lib\php.ini -f C:\Work\snippets\parser\snipit.php
-c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\snippets.py done
+C:\Work\snippets\parser\lib\php -c C:\Work\snippets\parser\lib\php.ini -f C:\Work\snippets\parser\snipit.php >> C:\Work\snippets\parser\parser.log
+C:\Work\doorscenter\doorsagents\snippets-done.bat
 exit
