72b3c9fe2ad6cc51e884cf21787a7c781a05f892
alex borisov <alex@altstone.com>
2011-09-08 16:25:32 +0400

    развитие

--- doorsagents/snippets-cron.bat
+++ doorsagents/snippets-cron.bat
@@ -1,2 +1,2 @@
-@c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\snippets.py cron >> C:\Work\doorscenter\doorsagents\snippets.log
+@c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\snippets.py cron >> C:\Work\doorscenter\doorsagents\snippets1.log
 exit
--- doorsagents/xrumer-cron.bat
+++ doorsagents/xrumer-cron.bat
@@ -1,2 +1,2 @@
-@c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\xrumer.py cron >> C:\Work\doorscenter\doorsagents\xrumer.log
+@c:\python27\python2.7.exe c:\work\doorscenter\doorsagents\xrumer.py cron >> C:\Work\doorscenter\doorsagents\xrumer1.log
 exit
