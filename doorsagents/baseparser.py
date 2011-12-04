0bc946b294b8a6b61f9980a342300881fd89d123
alex borisov <alex@altstone.com>
2011-12-04 14:11:35 +0400

    развитие

--- doorsagents/baseparser.py
+++ doorsagents/baseparser.py
@@ -2,9 +2,11 @@
 import urllib, re, time, threading, Queue
 
 '''Параметры'''
+threadsCount = 100
 baseFileName = 'LinksList id1.txt'
-maxThreadsCount = 100
-startTopic = 'http://studentcafeonline.com/viewtopic.php?f=3&t=107417'
+startTopicsList = '''http://studentcafeonline.com/viewtopic.php?f=3&t=81331
+http://studentcafeonline.com/viewtopic.php?f=3&t=108702
+http://studentcafeonline.com/viewtopic.php?f=3&t=108701'''.split('\n')
 featuresList = '''/action=profile;u=
 /forum.php?
 /guest/index.php
@@ -25,69 +27,80 @@ featuresList = '''/action=profile;u=
 /viewtopic.php?
 /yabb.pl?'''.split('\n')
 
-class Spider(threading.Thread):
-    def __init__(self, url, queue1, queue2):
+class BaseSpider(threading.Thread):
+    def __init__(self, queue):
         '''Инициализация'''
         threading.Thread.__init__(self)
-        self.url = url
-        self.queue1 = queue1
-        self.queue2 = queue2
-        self.queue2.put('1')
-        self.queue2.task_done()
+        self.queue = queue
     
     def run(self):
         global processedUrls, selectedDomains, selectedUrls
-        '''Читаем страницу и извлекаем из нее ссылки'''
-        urlsList = []
-        try:
-            print('Getting %s...' % self.url)
-            fd = urllib.urlopen(self.url)
-            html = fd.read()
-            urlsList = re.findall(r'href=[\'"](http[^\'"]*)[\'"]', html)
-            fd.close()
-        except Exception as error:
-            print('Error: %s' % error)
-        '''Ссылки фильтруем на соответствие признакам'''
-        urlsListFeatured = []
-        for url in urlsList:
-            for feature in featuresList:
-                if url.find(feature) >= 0:
-                    urlsListFeatured.append(url)
-                    break
-        '''Обрабатываем полученные ссылки'''
-        for url in urlsListFeatured:
-            if url in processedUrls:
-                continue
-            self.queue1.put(url)
-            self.queue1.task_done()
-            domain = url.replace('http://', '').replace('www.', '')
-            domain = domain[:domain.find('/')]
-            if domain not in selectedDomains:
-                selectedUrls.append(url)
-                selectedDomains.append(domain)
-            processedUrls.append(url)
-        '''Завершение работы потока'''
-        self.queue2.get()
-        print('Base size: %d. Queue size: %d. Threads count: %d.' % (len(selectedDomains), self.queue1.qsize(), self.queue2.qsize()))
+        while True:
+            '''Получаем ссылку из очереди'''
+            url = self.queue.get() 
+            '''Читаем страницу и извлекаем из нее ссылки'''
+            urlsList = []
+            try:
+                print('- getting %s' % url)
+                fd = urllib.urlopen(url)
+                html = fd.read()
+                urlsList = re.findall(r'href=[\'"](http[^\'"]*)[\'"]', html)
+                fd.close()
+            except Exception:
+                pass
+            '''Ссылки фильтруем на соответствие признакам'''
+            urlsListFeatured = []
+            for url in urlsList:
+                for feature in featuresList:
+                    if url.find(feature) >= 0:
+                        urlsListFeatured.append(url)
+                        break
+            '''Обрабатываем полученные ссылки'''
+            for url in urlsListFeatured:
+                if url in processedUrls:
+                    continue
+                self.queue.put(url)
+                domain = url.replace('http://', '').replace('www.', '')
+                domain = domain[:domain.find('/')]
+                if domain not in selectedDomains:
+                    selectedUrls.append(url)
+                    selectedDomains.append(domain)
+                processedUrls.append(url)
+            '''Завершение обработки ссылки'''
+            self.queue.task_done()
+
+class BaseMonitor(threading.Thread):
+    def __init__(self, queue):
+        '''Инициализация'''
+        threading.Thread.__init__(self)
+        self.queue = queue
+    
+    def run(self):
+        '''Каждые 10 секунд сохраняем базу в файл и выводим текущую информацию'''
+        lastActionTime = time.time()
+        while True:
+            if time.time() - lastActionTime > 5:
+                open(baseFileName, 'w').write('\n'.join(selectedUrls))
+                lastActionTime = time.time()
+                print('Base size: %d. Queue size: %d.' % (len(selectedDomains), self.queue.qsize()))
+            time.sleep(1)
 
 '''Инициализация'''
 processedUrls = []
 selectedDomains = []
 selectedUrls = []
-queuedUrls = Queue.Queue()
-queuedUrls.put(startTopic)
-runningThreads = Queue.Queue()
+queue = Queue.Queue()
+for startTopic in startTopicsList:
+    queue.put(startTopic)
 
-'''Бесконечный цикл обработки'''
-lastSavedTime = 0
-while True:
-    '''Соблюдаем максимальное число потоков'''
-    while runningThreads.qsize() > maxThreadsCount:
-        time.sleep(1)
-    '''Запускаем новый поток'''
-    Spider(queuedUrls.get(), queuedUrls, runningThreads).start()
-    '''Каждые 10 секунд сохраняем базу в файл'''
-    if time.time() - lastSavedTime > 10:
-        open(baseFileName, 'w').write('\n'.join(selectedUrls))
-        lastSavedTime = time.time()
-        print('*** Base saved ***')
+'''Запускаем потоки'''
+for _ in range(threadsCount):
+    thread = BaseSpider(queue)
+    thread.daemon = True
+    thread.start()
+thread = BaseMonitor(queue)
+thread.daemon = True
+thread.start()
+
+'''Ждем окончания работы'''
+queue.join()
