665134092323bf66f2d964d226d7605b08f09949
alex borisov <alex@altstone.com>
2012-01-19 19:16:18 +0400

    удалены старые дорген и парсер сниппетов

--- doorsagents/3rdparty/doorgen/lib/custom_macros.php
+++
@@ -1,8 +0,0 @@
-<?php	$custom_macros = array(
-	'{ANALYTICSID}' => '',
-	'{ANALYTICS}' => '',
-	'{PIWIKID}' => '62',
-	'{PIWIK}' => '<script type="text/javascript">var pkBaseURL = (("https:" == document.location.protocol) ? "https://searchpro.name/stat/" : "http://searchpro.name/stat/");document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));</script><script type="text/javascript">try {var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", {PIWIKID});piwikTracker.trackPageView();piwikTracker.enableLinkTracking();} catch( err ) {}</script><noscript><p><img src="http://searchpro.name/stat/piwik.php?idsite={PIWIKID}" style="border:0" alt="" /></p></noscript>',
-	'{FAKE}' => ''
-	);
-?>
